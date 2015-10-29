178ca1069ca7328a45ef4f37be079d798dcc9817
Julio Makdisse Saito <saitodisse@gmail.com>
2015-10-29 15:40:27 -0200

    index fix and tests

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/sender');
+module.exports = require('./lib/src/sender');
---
+++ spec/index_spec.js
@@ -0,0 +1,15 @@
+import h from './spec_helper';
+import Index from '../../index';
+
+describe('Index:', function() {
+
+  it("should Index exists", function() {
+    h.expect(Index).to.not.be.undefined;
+  });
+
+  it("should send() exists", function() {
+    var index = new Index();
+    h.expect(index.send).to.not.be.undefined;
+  });
+
+});
